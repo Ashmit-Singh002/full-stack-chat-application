203b66cde5a52c0056db8e378fd4bfd9be4ef7f9
Atulit23 <atulit23@gmail.com>
2025-05-01 20:54:26 +0530

    new

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-  withCredentials: true,
+  // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: "https://full-stack-chat-application-nii8.onrender.com/api",
+  // withCredentials: true,
 });
